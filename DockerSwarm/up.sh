6571cf55010a5d3b5e018e4f5fbd826ccdec6aa9
Phat Nguyen <it.phat.nguyen@gmail.com>
2022-01-11 16:10:57 +0700

    up server

---
+++ DockerSwarm/up.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+cd /Users/admin/Documents/Devops/DockerSwarm/master
+exec  vagrant halt
+
+cd /Users/admin/Documents/Devops/DockerSwarm/node1
+exec  vagrant halt
+
+cd /Users/admin/Documents/Devops/DockerSwarm/node2
+exec  vagrant halt
