7390acdb5b5ed4a21022cd7d5d4623f421107efa
Phat Nguyen <it.phat.nguyen@gmail.com>
2022-01-13 09:52:11 +0700

    node app + docker file

---
+++ Coding/nodejs/app.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var sleep = require("system-sleep");
+var os = require("os");
+
+http
+  .createServer((req, res) => {
+    sleep(1);
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Swarm service (Node app), hostname: " + os.hostname);
+  })
+  .listen(8085);
+
+console.log("Node app run on port 8085"+ os.hostname);
--- DockerSwarm/down.sh
+++ DockerSwarm/down.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 cd /Users/admin/Documents/Devops/DockerSwarm/master
-exec  vagrant up
-
-cd /Users/admin/Documents/Devops/DockerSwarm/node1 
-exec  vagrant up
-
+exec  vagrant halt
+&
+cd /Users/admin/Documents/Devops/DockerSwarm/node1
+exec  vagrant halt
+&
 cd /Users/admin/Documents/Devops/DockerSwarm/node2
-exec  vagrant up 
+exec  vagrant halt
